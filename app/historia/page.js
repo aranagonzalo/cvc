3ea58683b1e7c64ae2910bfa1600fefa10f0fd08
Gonzalo Arana <gonzaloaranam@gmail.com>
2024-03-25 18:39:06 -0500

    last

--- app/historia/page.js
+++ app/historia/page.js
@@ -76,85 +76,95 @@ const page = () => {
                     title="Fundación"
                 />
                 <StoryCardLeft
-                    text="Es la zona de concesión de distribución más desarrollada, dado que fue otorgada a CVC energía en el año 1995. En ese entonces, tan sólo existían 500 hectáreas cultivadas. Ahora, existen más de 40,000 hectáreas cultivadas que generan más de 160,000 puestos de trabajo, gracias al soporte energético que promovió CVC energía."
+                    text="Es la zona de concesión de distribución más desarrollada, dado que fue otorgada a CVC energía en el año 1995. En ese entonces, tan sólo existían 500 hectáreas cultivadas. Ahora, existen más de 40,000 hectáreas cultivadas que generan más de 200,000 puestos de trabajo, gracias al soporte energético que promovió CVC energía."
                     date="1995"
                     title="Concesión de Distribución - Villacurí"
                 />
                 <StoryCardRight
-                    text="El 5 de agosto de 1995, se realizó la puesta en servicio de la primera subestación: Coelvisac 1."
+                    text="5 de agosto de 1995, se realizó la puesta en servicio de la primera subestación: Coelvisac 1."
                     date="1995"
                     title="SET Coelvisac 1"
                 />
                 <StoryCardLeft
-                    text="Fue otorgada en el 2001. Cuenta con más de 21,000 hectáreas. Atiende suministros residenciales y operaciones productivas de agricultura."
+                    text="Fue otorgada en el 2001. Cuenta con más de 22,000 hectáreas. Atiende suministros residenciales y operaciones productivas de agricultura."
                     date="2001"
                     title="Concesión de Distribución - Andahuasi"
                 />
                 <StoryCardRight
-                    text="Pusimos en servicio la línea de transmisión Independencia Coelvisac 1 de 60 kV entre la Subestación Independencia y la subestación COELVISAC I, en el departamento de Ica."
+                    text="Concesión contractual en Motupe - Lambayeque hasta el año 2007."
+                    date="2005"
+                    title="Concesión de Distribución Motupe - Lambayeque"
+                />
+                <StoryCardLeft
+                    text="Pusimos en servicio la línea de transmisión Independencia Coelvisac 1 de 60 MW entre la Subestación Independencia y la subestación COELVISAC I, en el departamento de Ica."
                     date="2010"
                     title="LT Independencia Coelvisac 1 "
                 />
+                <StoryCardRight
+                    text="Operación y mantenimiento para Petrobras desde enero 2010 a julio 2017 en distrito de El Alto, provincia de Talara, departamento de Piura. "
+                    date="2010"
+                    title="Central Termoeléctrica El Alto"
+                />
                 <StoryCardLeft
-                    text="Fue puesta en operación comercial en septiembre del 2013 con una capacidad Instalada de 1.6 MW en el Distrito de Huaura, provincia de Sayan, departamento de Lima."
+                    text="Fue puesta en operación comercial en septiembre del 2013 en el Distrito de Huaura, provincia de Sayan, departamento de Lima."
                     date="2013"
                     title="Central Hidroeléctrica Coelvihidro I – Quipico"
                 />
                 <StoryCardRight
-                    text="Fue otorgada en el año 2014. Cuenta con un área de concesión de  más de 150,000 héctareas y una demanda actual de más de 30 MWA."
+                    text="Fue otorgada en enero del 2014. Cuenta con un área de concesión de más de 150,000 héctareas y una capacidad actual de más de 30 MWA."
                     date="2014"
-                    title="Concesión de Distribución - Lambayeque"
+                    title="Concesión de Distribución Olmos - Lambayeque: "
                 />
                 <StoryCardLeft
-                    text="El 30 de octubre del 2014, pusimos en servicio las SET Tierras Nuevas y SET Felam. Así como la línea de transmisión Felam Tierras Nuevas de 220 kV entre ambas subestaciones,  en el departamento de Lambayeque."
+                    text="El 30 de octubre del 2014, pusimos en servicio las SET Tierras Nuevas y SET Felam. Así como la línea de transmisión Felam Tierras Nuevas de 220 kV entre ambas subestaciones,  en el departamento de Lambayeque en la zona de Olmos."
                     date="2014"
                     title="Sistema de Transmisión FELAM y Tierras Nuevas"
                 />
                 <StoryCardRight
-                    text="Operación y mantenimiento desde enero 2010 a julio 2017 en distrito de El Alto, provincia de Talara, departamento de Piura."
-                    date="2017"
-                    title="Central Termoeléctrica El Alto"
-                />
-                <StoryCardLeft
-                    text="El 24 de febrero del 2019 pusimos en servicio la SE Huarango ubicada en Villacurí, Ica."
+                    text="El 24 de febrero del 2019 pusimos en servicio la SE Huarango ubicada en Villacurí, Ica. Esta subestación cuenta con una capacidad actual de 50 MVA."
                     date="2019"
                     title="SE Huarango"
                 />
-                <StoryCardRight
+                <StoryCardLeft
                     text="Fue puesta en operación comercial en  mayo del 2019, termoeléctrica de ciclo simple a gas natural con una capacidad Instalada: 9.99 MW en el distrito de Salas, provincia de Ica, departamento de Ica."
                     date="2019"
                     title="Central Termoeléctrica Villacurí"
                 />
-                <StoryCardLeft
-                    text="Pusimos en servicio la línea de transmisión de 60 kV entre la Subestación Tierras Nuevas y la Subestación Pampa Pañala, en el departamento de Lambayeque."
+                <StoryCardRight
+                    text="Pusimos en servicio la línea de transmisión de 60 kV entre la Subestación Tierras Nuevas y la Subestación Pampa Pañala, en el departamento de Lambayeque. Esto con el objetivo de reforzar el sistema del área demandada entre Pampa Pañala, Motupe y Chiclayo."
                     date="2020"
                     title="LT Tierras Nuevas - Pampa Pañala"
                 />
-                <StoryCardRight
-                    text="El 7 de febrero del 2021 pusimos en servicio la SE Lomas, ubicada en Villacurí – Ica. "
+                <StoryCardLeft
+                    text="El 7 de febrero de 2021, pusimos en servicio la Subestación Eléctrica (SE) Lomas, situada en Villacurí, Ica. Cuenta con una capacidad de 50 MVA y representa la tercera subestación en esta zona de concesión, lo que contribuye significativamente al fortalecimiento del sistema eléctrico en Villacurí."
                     date="2021"
                     title="SE Lomas"
                 />
-                <StoryCardLeft
-                    text="En el proyecto Tacna se estima una inversión de 12 millones de dólares y se extiende sobre 74,000 hectáreas. Tenemos el objetivo de suministrar energía eléctrica para impulsar proyectos de agroexportación que generen un mayor desarrollo en las comunidades y comercios."
+                <StoryCardRight
+                    text="El proyecto en Tacna tiene un valor estimado de 12 millones de dólares y se extiende sobre 74,000 hectáreas. Tenemos el objetivo de suministrar energía eléctrica para impulsar proyectos de agroexportación, aviculura, vivienda, agua que generen un mayor desarrollo en las comunidades y comercios."
                     date="2021"
                     title="Concesión de Distribución - Tacna "
                 />
-                <StoryCardRight
+                <StoryCardLeft
                     text="En junio del 2022, tras más de 20 años de operaciones, decidimos evolucionar y cambiar nuestro nombre comercial a CVC energía. Junto con ello, iniciamos la construcción de una imagen más moderna e innovadora, manteniendo nuestra esencia y el compromiso de potenciar el desarrollo de nuestro país."
                     date="2022"
                     title="Renovamos nuestra imagen"
                 />
-                <StoryCardLeft
+                <StoryCardRight
                     text="Fue puesta en operación comercial en  febrero del 2023, termoeléctrica de ciclo simple a gas natural con una capacidad Instalada: 9.95 MW en el distrito de Salas, provincia de Ica, departamento de Ica."
                     date="2023"
                     title="Central Termoeléctrica Salas"
                 />
-                <StoryCardRight
-                    last={true}
-                    text="En el año 2023, obtuvimos ambas concesiones de distribución.  En el proyecto Piura se estima una inversión de 13 millones de dólares y abarca 78,000 hectáreas, mientras que en el proyecto Moquegua se estima una inversión de 9 millones de dólares y se extiende sobre 215,000 hectáreas. Esta iniciativa nos permitirá suministrar electricidad a miles de hectáreas rurales que actualmente carecen de acceso a este servicio esencial."
+                <StoryCardLeft
+                    text="En el año 2023, obtuvimos ambas concesiones de distribución. El proyecto en Piura está estimado en 13 millones de dólares y abarca 78,000 hectáreas, mientras que el proyecto en Moquegua tiene un valor estimado de 9 millones de dólares y se extiende sobre 215,000 hectáreas. Esta iniciativa nos permitirá suministrar electricidad a miles de hectáreas rurales que actualmente carecen de acceso a este servicio esencial y promover un nuevo polo de desarrollo."
                     date="2023"
                     title="Concesiones de Distribución - Piura y Moquegua "
+                />
+                <StoryCardRight
+                    last={true}
+                    text="Iniciamos el proyecto de la Central Solar Fotovoltáica, la cual estará ubicada en Villacurí - Ica y tendrá una capacidad de hasta 10 MW."
+                    date="2024"
+                    title="Inicio del Proyecto Solar – Primera Fase"
                 />
             </section>
         </div>
